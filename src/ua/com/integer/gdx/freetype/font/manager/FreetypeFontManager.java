bc1151a1d2ea9adf16ab15826522159bfc7e5594
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2016-08-03 10:27:33 +0300

    Added AUTHORS. Added JavaDoc

--- src/ua/com/integer/gdx/freetype/font/manager/FreetypeFontManager.java
+++ src/ua/com/integer/gdx/freetype/font/manager/FreetypeFontManager.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package ua.com.integer.gdx.freetype.font.manager;
 
 import com.badlogic.gdx.Gdx;
@@ -6,6 +22,15 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.ObjectMap;
 
+/**
+ * Helper class to manage multiple FreeType (.ttf) fonts.
+ * 
+ * By default you should put all your .ttf files into android/assets/ttf-fonts folder.
+ * 
+ * Don't forget to call dispose() when you don't need instance of this class more.
+ *
+ * @author 1nt3g3r
+ */
 public class FreetypeFontManager implements Disposable {
     private String defaultChars = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯяabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;:,{}\"´`'<>’";
     private String fontsFolder = "ttf-fonts/";
@@ -15,46 +40,84 @@ public class FreetypeFontManager implements Disposable {
     private ObjectMap<String, FreeTypeFontGenerator> generators = new ObjectMap<String, FreeTypeFontGenerator>();
     private ObjectMap<String, BitmapFont> fonts = new ObjectMap<String, BitmapFont>();
 
+    /**
+     * Set folder where this class will look .ttf files
+     */
     public void setFontsFolder(String fontsFolder) {
         this.fontsFolder = fontsFolder;
     }
     
+    /**
+     * Return folder where this class looks .ttf files
+     */
     public String getFontsFolder() {
 		return fontsFolder;
 	}
     
+    /**
+     * @param defaultChars characters that will be used for font generation
+     */
     public void setDefaultChars(String defaultChars) {
 		this.defaultChars = defaultChars;
 	}
     
+    /**
+     * @return default characters that will be in generated fonts
+     */
     public String getDefaultChars() {
 		return defaultChars;
 	}
     
+    /**
+     * @param defaultFontName if you call loadFont(size) or getFont(size), this defaultFontName will be used.
+     * 
+     * It can be useful when you're using only one font.
+     */
     public void setDefaultFontName(String defaultFontName) {
 		this.defaultFontName = defaultFontName;
 	}
     
+    /**
+     * See setDefaultFontName
+     */
     public String getDefaultFontName() {
 		return defaultFontName;
 	}
     
+    /**
+     * Load font using defaultFontName. Equal to: loadFont(getDefaultFontName(), size)
+     */
     public void loadFont(int size) {
     	loadFont(defaultFontName, size);
     }
     
+    /**
+     * Load font with given font name and size
+     * @param name name of .ttf file (without .ttf extension)
+     */
     public void loadFont(String name, int size) {
         loadFont(name, size, new FreeTypeFontGenerator.FreeTypeFontParameter());
     }
 
+    /**
+     * Load font with given font name, size, and additional parameters
+     * @param name name of .ttf file (without .ttf extension)
+     * @param parameter specific font effects (shadow, border, etc). Look {@FreeTypeFontParameter} to get more
+     */
     public void loadFont(String name, int size, FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
         loadFont(name, size, defaultChars, parameter);
     }
     
+    /**
+     * Load font with given name, size and specific chars
+     */
     public void loadFont(String name, int size, String chars) {
     	loadFont(name, size, chars, new FreeTypeFontGenerator.FreeTypeFontParameter());
     }
 
+    /**
+     * Load font with given name, size, chars and specific font effects
+     */
     public void loadFont(String fontName, int fontSize, String chars, FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
         String fullFontName = getFullFontName(fontName, fontSize);
 
@@ -68,10 +131,17 @@ public class FreetypeFontManager implements Disposable {
         }
     }
     
+    /**
+     * Returns default font with given size. Equal to: getFont(getDefaultFontName(), size)
+     */
     public BitmapFont getFont(int fontSize) {
     	return getFont(defaultFontName, fontSize);
     }
 
+    /**
+     * Return font by given font name and font size. If font isn't loaded, it will be loaded by calling 
+     * loadFont(fontName, size)
+     */
     public BitmapFont getFont(String fontName, int fontSize) {
         String fullFontName = getFullFontName(fontName, fontSize);
         if (!fonts.containsKey(fullFontName)) {
@@ -90,6 +160,9 @@ public class FreetypeFontManager implements Disposable {
         return generators.get(name);
     }
 
+    /**
+     * Call this method to avoid memory leaks. Typically you should do it in your dispose() method of your game.
+     */
     @Override
     public void dispose() {
         for(BitmapFont font : fonts.values()) {
@@ -100,6 +173,9 @@ public class FreetypeFontManager implements Disposable {
         unloadAllGenerators();
     }
     
+    /**
+     * Print debug info. How many generators and fonts are loaded, and their names. Can be useful for debugging.
+     */
     public void printInfo() {
     	System.out.println("Loaded generator count: " + generators.size + ". Loaded font generator names:");
     	for(String generatorName : generators.keys()) {
@@ -112,6 +188,10 @@ public class FreetypeFontManager implements Disposable {
     	}
     }
     
+    /**
+     * Unloads generator with given name. It can be useful if you don't need this generator more (you will not 
+     * create more fonts using this generator).
+     */
     public void unloadGenerator(String generatorName) {
     	if (generators.containsKey(generatorName)) {
     		generators.get(generatorName).dispose();
@@ -119,6 +199,9 @@ public class FreetypeFontManager implements Disposable {
     	}
     }
     
+    /**
+     * Unload all generators. See unloadGenerator(String generatorName);
+     */
     public void unloadAllGenerators() {
     	for(FreeTypeFontGenerator generator : generators.values()) {
     		generator.dispose();
@@ -126,6 +209,9 @@ public class FreetypeFontManager implements Disposable {
     	generators.clear();
     }
     
+    /**
+     * Unloads font with given name. It can be useful if you don't need this font.
+     */
     public void unloadFont(String fontName, int fontSize) {
     	String fullFontName = getFullFontName(fontName, fontSize);
 
